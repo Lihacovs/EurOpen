4e7aa97d38f1533432ea30872a1cbdbebc810d57
Lihacovs <k.lihacovs@inbox.lv>
2019-10-28 20:42:50 +0000

    about

--- app/src/main/java/eu/balticit/europen/ui/about/AboutFragment.kt
+++ app/src/main/java/eu/balticit/europen/ui/about/AboutFragment.kt
@@ -15,14 +15,15 @@
 
 package eu.balticit.europen.ui.about
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.google.android.material.snackbar.Snackbar
 import eu.balticit.europen.R
 import eu.balticit.europen.data.prefs.AppSharedPrefs
 import eu.balticit.europen.ui.intro.IntroActivity
@@ -45,11 +46,53 @@ class AboutFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val watchIntro: ImageView = view.findViewById(R.id.iv_about_link_functions)
-        watchIntro.setOnClickListener { v ->
+        //Opens app introduction screens
+        val watchIntroIv: ImageView = view.findViewById(R.id.iv_about_watch_intro)
+        watchIntroIv.setOnClickListener {
             mPrefs.watchAppIntro(false)
             val intent = Intent(activity, IntroActivity::class.java)
             startActivity(intent)
+        }
+
+        //Opens email to send to developer
+        val sendEmailIv: ImageView = view.findViewById(R.id.iv_about_send_email)
+        sendEmailIv.setOnClickListener {
+            val emailIntent = Intent(
+                Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto", getString(R.string.app_email), null
+                )
+            )
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+            emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_body))
+            startActivity(
+                Intent.createChooser(
+                    emailIntent,
+                    getString(R.string.email_chooser_title)
+                )
+            )
+        }
+
+        //Opens GooglePlay to rate app
+        val rateAppIv: ImageView = view.findViewById(R.id.iv_about_send_rate)
+        rateAppIv.setOnClickListener {
+            val appPackageName: String = context!!.packageName
+            try {
+                activity!!.startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW, Uri.parse(
+                            activity!!.resources.getString(R.string.app_market_link) + appPackageName
+                        )
+                    )
+                )
+            } catch (e: ActivityNotFoundException) {
+                activity!!.startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW, Uri.parse(
+                            activity!!.resources.getString(R.string.app_google_play_store_link) + appPackageName
+                        )
+                    )
+                )
+            }
         }
     }
 }
