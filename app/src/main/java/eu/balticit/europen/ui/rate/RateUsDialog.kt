da47b91be3cf417fbcdfcb011918510b81a3f586
Lihacovs <k.lihacovs@inbox.lv>
2019-10-30 20:09:23 +0000

    rate

--- app/src/main/java/eu/balticit/europen/ui/rate/RateUsDialog.kt
+++ app/src/main/java/eu/balticit/europen/ui/rate/RateUsDialog.kt
@@ -15,20 +15,76 @@
 
 package eu.balticit.europen.ui.rate
 
+import android.graphics.PorterDuff
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import eu.balticit.europen.R
+import java.util.*
 
-class RateUsDialog: DialogFragment() {
+class RateUsDialog : DialogFragment() {
+
+    private lateinit var mRatingMessageView: View
+    private lateinit var mPlayStoreRatingView: View
+    private lateinit var mRatingBar: RatingBar
+    private lateinit var mSubmitButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.dialog_rate_us, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mRatingMessageView = view.findViewById(R.id.view_rating_message)
+        mPlayStoreRatingView = view.findViewById(R.id.view_play_store_rating)
+        mRatingBar = view.findViewById(R.id.rating_bar_feedback)
+        mSubmitButton = view.findViewById(R.id.btn_submit)
+        setUp(view)
+    }
+
+    private fun setUp(view: View) {
+        mRatingMessageView.visibility = View.GONE
+        mPlayStoreRatingView.visibility = View.GONE
+
+        val stars: LayerDrawable = mRatingBar.progressDrawable as LayerDrawable
+        stars.getDrawable(2).setColorFilter(
+            ContextCompat.getColor(
+                Objects.requireNonNull(context)!!,
+                R.color.colorAccent
+            ),
+            PorterDuff.Mode.SRC_ATOP
+        )
+
+        stars.getDrawable(0).setColorFilter(
+            ContextCompat.getColor(
+                Objects.requireNonNull(context)!!,
+                R.color.shadow
+            ),
+            PorterDuff.Mode.SRC_ATOP
+        )
+
+        stars.getDrawable(1).setColorFilter(
+            ContextCompat.getColor(
+                Objects.requireNonNull(context)!!,
+                R.color.shadow
+            ),
+            PorterDuff.Mode.SRC_ATOP
+        )
+
+        mSubmitButton.setOnClickListener {
+            Toast.makeText(activity, "Rating: " + mRatingBar.rating, Toast.LENGTH_SHORT).show()
+        }
     }
 }
